73ab100d4a48a3fa928c9a5f91a5909962a31eaf
Devan Sisson <devan.sisson@outlook.com>
2015-07-31 04:04:59 -0600

    First commit

---
+++ SlipSheetPreventionApp/bin/Android/Debug/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Devan\Documents\Visual Studio 2015\Projects\SlipSheetPreventionApp\SlipSheetPreventionApp\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Devan\Documents\Visual Studio 2015\Projects\SlipSheetPreventionApp\SlipSheetPreventionApp\platforms\android\AndroidManifest.xml \
---
+++ SlipSheetPreventionApp/bin/Android/Debug/MainActivity-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\Devan\Documents\Visual Studio 2015\Projects\SlipSheetPreventionApp\SlipSheetPreventionApp\platforms\android\ant-build\MainActivity-debug-unaligned.apk : \
+C:\Users\Devan\Documents\Visual Studio 2015\Projects\SlipSheetPreventionApp\SlipSheetPreventionApp\platforms\android\ant-build\MainActivity.ap_ \
+C:\Users\Devan\Documents\Visual Studio 2015\Projects\SlipSheetPreventionApp\SlipSheetPreventionApp\platforms\android\ant-build\classes.dex \
+C:\Users\Devan\Documents\Visual Studio 2015\Projects\SlipSheetPreventionApp\SlipSheetPreventionApp\platforms\android\CordovaLib\ant-build\classes.jar \
